12f86b2e003d991338c401980587a5e00dfe3b37
Jorge <jorge.toledo.6@hotmail.com>
2022-09-06 12:41:15 -0300

    VIRTUAL

---
+++ Ejercicios/Ejercicio1.py
@@ -0,0 +1,10 @@
+#Identificar y generar los modelos de clases para crear un sistema que administre un taller mecanico.
+#Es decir, las reparaciones que en este se generan, los repuestos que se utilizan, los autos que ingresan 
+#los mecanicos que hacen las reparaciones y los clientes a los que se atienden.
+
+#Solo se pide nombre de la class y atributos mas importantes. Ejem:
+
+class Persona:
+    rut = ""
+    nombres = ""
+    apellidos = ""
