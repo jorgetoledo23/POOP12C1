2a4aeed17e4358ef810670a7a20363dd17132484
Jorge <jorge.toledo.6@hotmail.com>
2022-09-20 17:19:18 -0300

    Tienda

---
+++ Tienda.py
@@ -0,0 +1,17 @@
+from Model.Item import *
+
+Tienda = []
+
+Item1 = Item("Espada", 0, 120, 300)
+Item2 = Item("Escudo", 120, 0, 300)
+Item3 = Item("Pocion de Ataque", 0, 50, 150)
+Item4 = Item("Pocion de Defenza", 100, 0, 150)
+Item5 = Item("Armadura", 300, 50, 400)
+Item6 = Item("Daga", 0, 80, 100)
+
+Tienda.append(Item1)
+Tienda.append(Item2)
+Tienda.append(Item3)
+Tienda.append(Item4)
+Tienda.append(Item5)
+Tienda.append(Item6)
